fe7744b7088cb809e88d03fc44231f70872f80f9
zfurtak <92310164+zfurtak@users.noreply.github.com>
2023-05-01 22:16:35 +0200

    Delete exercise_3 directory

--- exercise_3/src/Exercise03/CardinalDirections.java
+++
@@ -1,9 +0,0 @@
-package Exercise03;
-
-public enum CardinalDirections {
-	North, South;
-
-	public static CardinalDirections getRandomDirection() {
-		return (Runway.rnd.nextBoolean())? North : South;
-	}
-}
